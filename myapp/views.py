6cb0125e9ba6204b82ca77387d460ae7f69da0a1
Tarik Ouabrk <tarik.ouabrk@gmail.com>
2026-02-01 10:10:09 +0100

    done

--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>hello i am tarik</h1>")
+
+def about(request):
+    return HttpResponse("<h2>i am here </h1>")
