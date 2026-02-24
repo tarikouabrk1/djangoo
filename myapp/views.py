2ea292f780888159869652ef21623e4a2abcdf4e
Tarik Ouabrk <tarik.ouabrk@gmail.com>
2026-02-24 03:52:11 +0000

    done

--- myapp/views.py
+++ myapp/views.py
@@ -6,16 +6,14 @@ def resource_list(request):
     return render(request, 'myapp/resource_list.html', {'resources': resources})
 
 def resource_detail(request, pk):
-    # Get the resource object or return 404 if not found
     resource = get_object_or_404(Resource, pk=pk)
-    
-    # For LaTeX or code files, read the content so we can render it in template
+
     if resource.resource_type in ['latex', 'code']:
-        resource_content = resource.file.read().decode('utf-8')
+        with resource.file.open('rb') as f:
+            resource_content = f.read().decode('utf-8')
     else:
         resource_content = ''
-    
-    # Pass both the resource and its content to the template
+
     return render(request, 'myapp/resource_detail.html', {
         'resource': resource,
         'content': resource_content
